720ba06aa1d1accdced84292df34b14e3483c8fe
daniloccangucu <daniloccangucu@gmail.com>
2023-11-09 12:08:20 +0200

    text challenge included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,25 +13,25 @@ import { RecaptchaModule } from 'ng-recaptcha';
 
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CaptchaComponent,
-    ResultComponent
+    ResultComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgHcaptchaModule.forRoot({
       siteKey: 'faf6905d-c3d6-4694-a69f-726b6b0e8f78',
-      languageCode: 'en'
+      languageCode: 'en',
     }),
     RecaptchaModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/captcha/captcha.component.ts
+++ src/app/captcha/captcha.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-captcha',
   templateUrl: './captcha.component.html',
-  styleUrls: ['./captcha.component.css']
+  styleUrls: ['./captcha.component.css'],
 })
 export class CaptchaComponent {
-  progress = 0;
+  progress: number = 0;
+  challengeType: 'recaptcha' | 'hcaptcha' | 'text' = 'recaptcha';
+  challengeStrings: string[] = [];
+  userInput: string = '';
 
   constructor(private router: Router) {
     const storedProgress = localStorage.getItem('progress');
     if (storedProgress) {
       this.progress = Number(storedProgress);
     }
+    this.generateChallengeStrings();
   }
 
   ngOnInit() {
@@ -26,16 +30,51 @@ export class CaptchaComponent {
   private updateProgressInLocalStorage() {
     localStorage.setItem('progress', this.progress.toString());
   }
-  
-  updateRecaptchaWin() {
-    this.progress = 50;
+
+  updateChallengeWin() {
+    if (
+      this.challengeType === 'recaptcha' ||
+      this.challengeType === 'hcaptcha'
+    ) {
+      this.progress += 33;
+    } else {
+      this.progress += 34;
+    }
+    this.switchChallenge();
     this.updateProgressInLocalStorage();
   }
 
-  updateHcaptchaWin() {
-    this.progress = 100;
-    this.updateProgressInLocalStorage();
-    this.router.navigate(['/result']);
+  generateChallengeStrings() {
+    this.challengeStrings.push(this.generateRandomString(6)); // Change 6 to the desired length of your string
   }
 
+  generateRandomString(length: number): string {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return result;
+  }
+
+  checkTextChallenge() {
+    if (this.userInput === this.challengeStrings[0]) {
+      this.updateChallengeWin();
+      this.userInput = '';
+    }
+  }
+
+  switchChallenge() {
+    if (this.challengeType === 'recaptcha') {
+      this.challengeType = 'hcaptcha';
+    } else if (this.challengeType === 'hcaptcha') {
+      this.challengeType = 'text';
+    } else {
+      this.router.navigate(['/result']);
+    }
+    this.updateProgressInLocalStorage();
+  }
 }
